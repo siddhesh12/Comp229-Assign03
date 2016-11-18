deddd2ff971b64620678208f92f3dacfe5e6cd1d
Siddhesh <siddheshm1212@gmail.com>
2016-11-18 15:59:40 -0800

    Implemented delete student in student page

--- Comp229-Assign03/Student.aspx.cs
+++ Comp229-Assign03/Student.aspx.cs
@@ -45,7 +45,35 @@ namespace Comp229_Assign03
         }
 
         
-        protected void updateData(string lastName, string firstMidName, int courseID, string courseTitle, string grade, int courseCredits)
+        protected void delete()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Comp229Assign03ConnectionString"].ToString());
+            SqlCommand commEnrollment = new SqlCommand("delete from Enrollments WHERE StudentID=@StudentID", connection);
+            SqlCommand commStudent = new SqlCommand("delete from Students WHERE StudentID=@StudentID", connection);
+
+            commEnrollment.Parameters.Add("@StudentID", System.Data.SqlDbType.Int);
+            commEnrollment.Parameters["@StudentID"].Value = Convert.ToInt32(Request.QueryString["Name"]);
+
+            commStudent.Parameters.Add("@StudentID", System.Data.SqlDbType.Int);
+            commStudent.Parameters["@StudentID"].Value = Convert.ToInt32(Request.QueryString["Name"]);
+
+            try
+            {
+                connection.Open();
+                commEnrollment.ExecuteNonQuery();
+                commStudent.ExecuteNonQuery();
+                Response.Redirect("home.aspx");
+            }   
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('error: " + ex.Message + "'); </script>");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        protected void updateStudent(string lastName, string firstMidName)
         {
             SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Comp229Assign03ConnectionString"].ToString());
             SqlCommand comm = new SqlCommand("UPDATE Students SET FirstMidName=@FirstMidName, LastName=@LastName WHERE StudentID=@StudentID", connection);
@@ -104,7 +132,7 @@ namespace Comp229_Assign03
             string firstMidName = ((TextBox)(row.Cells[3].Controls[0])).Text;
             string courseTitle = row.Cells[5].Text;
             string courseCredits = row.Cells[6].Text;
-            updateData(lastName, firstMidName, 0, "ss", "", 0);
+            updateStudent(lastName, firstMidName);
 
             GridView1.EditIndex = -1;
 
@@ -126,6 +154,10 @@ namespace Comp229_Assign03
                 Response.Write("<script>alert('failed');</script>");
             }
 
+        }
+        protected void student_GridView_RowDeleting (object sender, GridViewDeleteEventArgs e)
+        {
+            delete();
         }
     }
 }
